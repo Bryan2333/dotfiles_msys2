220a8f2efa4888126af4f63dced128d1e45b606a
BryanLiang <liangrui.ch@gmail.com>
2026-03-06 10:02:14 +0800

    更新bash和fish的配置

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,30 +1,10 @@
 if status is-interactive
     # 环境变量 {{{
-    ## XDG目录
-    set -gx XDG_CACHE_HOME  $HOME/.cache
-    set -gx XDG_CONFIG_HOME $HOME/.config
-    set -gx XDG_DATA_HOME   $HOME/.local/share
-    set -gx XDG_STATE_HOME  $HOME/.local/state
 
-    set -gx BAT_CONFIG_PATH $XDG_CONFIG_HOME/bat/config
-    set -gx WGETRC          $XDG_CONFIG_HOME/wgetrc
-
-    ## nvm
-    set -gx nvm_data    $XDG_DATA_HOME/nvm
-    set -gx nvm_mirror  https://mirrors.ustc.edu.cn/node
-
-    ## 默认编辑器
-    set -gx EDITOR vim
-    set -gx VISUAL $EDITOR
-
-    ## FZF默认命令
-    set -gx FZF_DEFAULT_COMMAND "find -xdev \! \( -path '*/.git' -prune \) -printf '%P\n'"
-
-    ## less命令显示PUA
-    set -gx LESSUTFCHARDEF E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p
-
-    ## 扩展PATH
-    fish_add_path -a $XDG_DATA_HOME/npm $HOME/.local/bin
+    # VSCode集成
+    if string match -q "$TERM_PROGRAM" "vscode"
+         . (code --locate-shell-integration-path fish)
+    end
     # }}}
 
     # 缩写 {{{
